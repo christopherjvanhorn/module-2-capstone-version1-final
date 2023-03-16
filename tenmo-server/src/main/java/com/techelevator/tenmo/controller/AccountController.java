f4a263e0126d8c24e0d8468e087deabc8e903723
Christopher Van Horn <christopherjvanhorn3@gmail.com>
2023-03-16 16:35:13 -0400

    removed permit-all that should not have been

--- tenmo-server/src/main/java/com/techelevator/tenmo/controller/AccountController.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/controller/AccountController.java
@@ -7,9 +7,6 @@ import org.springframework.http.HttpStatus;
 import com.techelevator.tenmo.model.Transfer;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
-import com.techelevator.tenmo.dao.*;
-import com.techelevator.tenmo.model.*;
-import org.springframework.http.HttpStatus;
 import com.techelevator.tenmo.dao.UserDao;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -18,11 +15,14 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * AccountController class handles all the requests from the
+ * client side to the end points of "/transfer".
+ */
 @RestController
 @RequestMapping("/transfer")
 @PreAuthorize("isAuthenticated()")
 public class AccountController {
-    // TODO Add Dao & Dto
     TransferDao transferDao;
     AccountDao accountDao;
     UserDao userDao;
@@ -34,8 +34,12 @@ public class AccountController {
         this.userDao = userDao;
     }
 
+    /**
+     * getAccountByUserId returns account for the user with the given userId.
+     * @param userId
+     * @return Account
+     */
     @GetMapping(path = "/{userId}")
-    @PreAuthorize("permitAll")
     public Account getAccountByUserId(@PathVariable int userId) {
         Account account = accountDao.getAccountByUserId(userId);
         if (account == null) {
@@ -45,6 +49,11 @@ public class AccountController {
         }
     }
 
+    /**
+     * viewPendingRequests returns all pending requests for given userId.
+     * @param userId
+     * @return List<TransferDto>
+     */
     @GetMapping("/pending/{userId}")
     public List<TransferDto> viewPendingRequests(@PathVariable Integer userId) {
         List<TransferDto> pendingTransfers = null;
@@ -56,22 +65,39 @@ public class AccountController {
         }
     }
 
-    @PutMapping("{transferId}")
-    public boolean pendingTransferApproval (@PathVariable Integer transferId, boolean approval) {
+    /**
+     * pendingTransferApproval used to set status for pending approval
+     * if transfer is approved true is returned.
+     * @param transferId
+     * @return boolean
+     */
+    @PutMapping("/approved/{transferId}")
+    public void pendingTransferApproval (@PathVariable Integer transferId) {
         Transfer newTransfer = transferDao.getPendingTransfersByTransferId(transferId);
         //if approved: set trans.status to approved, update both account balance
-        if (approval){
-            newTransfer.setTransferStatus(2); //Approved
-            transferDao.updateTransferRequest(newTransfer); //update transfer db
-            sendBucks(accountDao.getAccountByAccountId(newTransfer.getAccountFrom()).getUserId(), accountDao.getAccountByAccountId(newTransfer.getAccountTo()).getUserId(), newTransfer.getAmount());
-            return true;
-        } else {
-            newTransfer.setTransferStatus(3); //Rejected
-            transferDao.updateTransferRequest(newTransfer); //update transfer db
-        }
-        return false;
+        newTransfer.setTransferStatus(2); //Approved
+        transferDao.updateTransferRequest(newTransfer); //update transfer db
+        sendBucks(accountDao.getAccountByAccountId(newTransfer.getAccountFrom()).getUserId(),
+                accountDao.getAccountByAccountId(newTransfer.getAccountTo()).getUserId(), newTransfer.getAmount());
     }
 
+    @PutMapping("/rejected/{transferId}")
+    public void pendingTransferRejection (@PathVariable Integer transferId) {
+        Transfer newTransfer = transferDao.getPendingTransfersByTransferId(transferId);
+        newTransfer.setTransferStatus(3); //Rejected
+        transferDao.updateTransferRequest(newTransfer); //update transfer db
+
+    }
+
+
+    /**
+     * sendBucks handles the logic for the current user sending
+     * bucks to a different user. Cannot be used to send to self.
+     * @param senderUserId
+     * @param userIdToSendTo
+     * @param amount
+     * @return boolean : true if successful
+     */
     @PostMapping("send/{senderUserId}/{userIdToSendTo}/{amount}")
     public boolean sendBucks(@PathVariable Integer senderUserId,
                              @PathVariable Integer userIdToSendTo,
@@ -84,12 +110,21 @@ public class AccountController {
             receiver.setBalance(receiver.getBalance().add(amount));
             accountDao.updateAccount(sender);
             accountDao.updateAccount(receiver);
+            if (transferDao.getTransfersByUserId(sender.getUserId()).isEmpty()) {
+                transferDao.createTransferRequest(new Transfer(2, 2, sender.getAccountId(), receiver.getAccountId(), amount ));
+            }
         } else {
             throw new IllegalArgumentException("Not enough available funds to send.");
         }
         return true;
     }
 
+    /**
+     * createTransfer recieves new transfer to create and handles
+     * logic to check if transfer amount is greater than 0.
+     * @param transfer
+     * @return
+     */
     @PostMapping()
     public Transfer createTransfer(@Valid @RequestBody Transfer transfer) {
         if (transfer.getAmount().compareTo(BigDecimal.ZERO) < 0) {
@@ -99,12 +134,22 @@ public class AccountController {
         return transferDao.createTransferRequest(transfer);
     }
 
-    @GetMapping("/users")
+    /**
+     * getUsers is used to get all of the current users int the database.
+     * @return List<User>
+     */
     @PreAuthorize("permitAll")
+    @GetMapping("/users")
     public List<User> getUsers(){
         return userDao.findAll();
     }
 
+    /**
+     * viewTransferHistory is used to retrieve all the past transfers
+     * for the current user.
+     * @param id
+     * @return List<TransferDto>
+     */
     @GetMapping("/history/users/{id}")
     public List<TransferDto> viewTransferHistory(@PathVariable int id) {
         List<TransferDto> transfersDto = transferDao.getTransfersByUserId(id);
@@ -123,8 +168,5 @@ public class AccountController {
         }
         return transferDtoWithNames;
     }
-
-
-
 
 }
